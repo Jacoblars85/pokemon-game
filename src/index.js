838bbbf18f83469d0df243652e0fb17af070a4f7
Jacob Larson <hockeybobber@comcast.net>
2025-02-18 20:27:03 -0600

    deleted the old mui components from old project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 // import lakeBackground from "./img/backgroundImg/LakeBackground.png";
 // import forestBackground from "./img/backgroundImg/RockForest.webp";
 
-// import ListItemText from "@mui/material/ListItemText";
-// import ListItem from "@mui/material/ListItem";
-// import List from "@mui/material/List";
-// import Divider from "@mui/material/Divider";
-// import AppBar from "@mui/material/AppBar";
-// import Toolbar from "@mui/material/Toolbar";
-// import IconButton from "@mui/material/IconButton";
-// import Typography from "@mui/material/Typography";
-// import CloseIcon from "@mui/icons-material/Close";
-// import Slide from "@mui/material/Slide";
-// import Box from "@mui/material/Box";
 // import battleMusic from "../../audio/battleMusic.mp3";
 
 // starter stats/info
