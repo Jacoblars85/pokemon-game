76940c1254fccddaa4c0a6d9aceed7e22d12dfcc
Jacob Larson <hockeybobber@comcast.net>
2025-02-20 23:18:57 -0600

    removed old sql

--- server/routes/character.router.js
+++ server/routes/character.router.js
@@ -4,41 +4,6 @@ const pool = require('../modules/pool')
 
 router.get('/user/characters', (req, res) => {
     // console.log('im in character get');
-//     const query = `
-//     SELECT "user_characters"."id" as "id",
-//     "user_characters"."user_id" as "user_id",
-//     "user_characters"."character_id",
-//     "user_characters"."starter_1",
-//     "user_characters"."starter_2",
-//     "user_characters"."new",
-//     "user_characters"."nickname",
-//     "characters"."name",
-//     "characters"."profile_pic",
-//     "characters"."hp",
-//     "characters"."stamina",
-//     "characters"."speed",
-//     "characters"."unique_attack",
-//     "characters"."unique_damage",
-//     "characters"."unique_stamina",
-//     "characters"."battle_pic",
-//     "items"."id" as "item_id",
-//     "items"."name" as "item_name",
-//     "items"."hp" as "item_hp",
-//     "items"."stamina" as "item_stamina",
-//     "items"."pic" as "item_pic",
-//     "items"."type" as "item_type",
-//     "items"."speed" as "item_speed",
-//     "items"."attack" as "item_attack",
-//     "items"."cost" as "item_cost",
-//     "items"."color" as "item_color"
-// FROM "user_characters" 
-// INNER JOIN "characters"
-//     ON "user_characters"."character_id" = "characters"."id"
-// LEFT JOIN "items"
-//     ON "user_characters"."item_id" = "items"."id"
-// WHERE "user_id" = $1
-// ORDER BY "character_id", "id" ASC;
-//   `;
 
 const query = `
 SELECT "user_characters"."id" as "id",
