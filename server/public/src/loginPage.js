d793f5fde08553fcfaa1a9c2583b4e4e87d5d9d6
Jacob Larson <hockeybobber@comcast.net>
2025-02-27 22:33:57 -0600

    cleaned up code and merged the page and form

--- server/public/src/loginPage.js
+++ server/public/src/loginPage.js
@@ -1,46 +1,4 @@
-import React from "react";
-import LoginForm from "../LoginForm/LoginForm";
-import { useHistory } from "react-router-dom";
-
-function LoginPage() {
-  const history = useHistory();
-
-  return (
-    <>
-      <div>
-
-        <LoginForm />
-
-        <center>
-          <button
-            type="button"
-            className="btn btn_asLink"
-            onClick={() => {
-              history.push("/registration");
-            }}
-          >
-            Register
-          </button>
-        </center>
-      </div>
-    </>
-  );
-}
-
-export default LoginPage;
-
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
-import TextField from '@mui/material/TextField';
-
-function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const errors = useSelector(store => store.errors);
-  const dispatch = useDispatch();
-
-  const login = (event) => {
+const login = (event) => {
     event.preventDefault();
 
     if (username && password) {
@@ -51,13 +9,25 @@ function LoginForm() {
           password: password,
         },
       });
+
+
+
+
     } else {
       dispatch({ type: 'LOGIN_INPUT_ERROR' });
     }
   }; // end login
 
-  return (
-    <form className="formPanel" onSubmit={login}>
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const errors = useSelector(store => store.errors);
+
+
+
+      <div>
+
+      <form className="formPanel" onSubmit={login}>
           <h2 style={{ margin: "5px", fontSize: "35px", textShadow: "2px 2px 2px gray"}}>Login</h2>
           <div style={{ height: "25px", width: "100%"}}>
       {errors.loginMessage && (
@@ -98,6 +68,18 @@ function LoginForm() {
         <input className="btn" type="submit" name="submit" value="Log In" />
       </div>
     </form>
-  );
-}
+
+        <center>
+          <button
+            type="button"
+            className="btn btn_asLink"
+            onClick={() => {
+              history.push("/registration");
+            }}
+          >
+            Register
+          </button>
+        </center>
+      </div>
+
 
