b27bdd5b024cf5e10016e33b8af123ec4b819d1e
Jacob Larson <hockeybobber@comcast.net>
2025-02-27 22:29:01 -0600

    formatted

--- server/public/src/registerPage.js
+++ server/public/src/registerPage.js
@@ -1,99 +1,98 @@
 const registerUser = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    dispatch({
-      type: 'REGISTER',
-      payload: {
-        username: username,
-        password: password,
-      },
-    });
-    function* registerUser(action) {
-        try {
-          // clear any existing error on the registration page
-          yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-      
-          // passes the username and password from the payload to the server
-          yield axios.post('/api/user/register', action.payload);
-      
-          // automatically log a user in after registration
-          yield put({ type: 'LOGIN', payload: action.payload });
-      
-          // set to 'login' mode so they see the login screen
-          // after registration or after they log out
-          yield put({ type: 'SET_TO_LOGIN_MODE' });
-        } catch (error) {
-          console.log('Error with user registration:', error);
-          yield put({ type: 'REGISTRATION_FAILED' });
-        }
-      }
-      
-  }; 
+  dispatch({
+    type: "REGISTER",
+    payload: {
+      username: username,
+      password: password,
+    },
+  });
+  function* registerUser(action) {
+    try {
+      // clear any existing error on the registration page
+      yield put({ type: "CLEAR_REGISTRATION_ERROR" });
 
+      // passes the username and password from the payload to the server
+      yield axios.post("/api/user/register", action.payload);
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const errors = useSelector((store) => store.errors);
+      // automatically log a user in after registration
+      yield put({ type: "LOGIN", payload: action.payload });
 
+      // set to 'login' mode so they see the login screen
+      // after registration or after they log out
+      yield put({ type: "SET_TO_LOGIN_MODE" });
+    } catch (error) {
+      console.log("Error with user registration:", error);
+      yield put({ type: "REGISTRATION_FAILED" });
+    }
+  }
+};
 
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const errors = useSelector((store) => store.errors);
 
-    <div>
-
-<form className="formPanel" onSubmit={registerUser}>
-      <h2 style={{ margin: "5px", fontSize: "35px", textShadow: "3px 3px 3px gray"}}>Register User</h2>
-      <div style={{ height: "25px", width: "100%"}}>
+<div>
+  <form className="formPanel" onSubmit={registerUser}>
+    <h2
+      style={{
+        margin: "5px",
+        fontSize: "35px",
+        textShadow: "3px 3px 3px gray",
+      }}
+    >
+      Register User
+    </h2>
+    <div style={{ height: "25px", width: "100%" }}>
       {errors.loginMessage && (
         <h3 className="alert" role="alert">
           {errors.loginMessage}
         </h3>
       )}
-</div>
-      <div>
-        <label htmlFor="username">
-          <input
-           margin="dense"
-           variant="standard"
-            type="text"
-            name="username"
-            label="username"
-            value={username}
-            required
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          <input
-           margin="dense"
-           variant="standard"
-            type="password"
-            name="password"
-            label="password"
-            value={password}
-            required
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Register" />
-      </div>
-    </form>
-
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push("/login");
-          }}
-        >
-          Login
-        </button>
-      </center>
     </div>
+    <div>
+      <label htmlFor="username">
+        <input
+          margin="dense"
+          variant="standard"
+          type="text"
+          name="username"
+          label="username"
+          value={username}
+          required
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+    </div>
+    <div>
+      <label htmlFor="password">
+        <input
+          margin="dense"
+          variant="standard"
+          type="password"
+          name="password"
+          label="password"
+          value={password}
+          required
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+    </div>
+    <div>
+      <input className="btn" type="submit" name="submit" value="Register" />
+    </div>
+  </form>
 
-
-
-
+  <center>
+    <button
+      type="button"
+      className="btn btn_asLink"
+      onClick={() => {
+        history.push("/login");
+      }}
+    >
+      Login
+    </button>
+  </center>
+</div>;
