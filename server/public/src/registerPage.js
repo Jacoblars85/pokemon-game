4658e0392b37cd2d933b8f4c042a9f2a91eada7f
Jacob Larson <hockeybobber@comcast.net>
2025-02-28 21:58:11 -0600

    formatted

--- server/public/src/registerPage.js
+++ server/public/src/registerPage.js
@@ -10,29 +10,27 @@ const registerUser = (event) => {
     method: "POST",
     url: "/api/user/register",
     data: {
-        username: username,
-        password: password,
-      },
+      username: username,
+      password: password,
+    },
   })
     .then((response) => {
-      console.log('register worked?', response);
-      
+      console.log("register worked?", response);
     })
     .catch((err) => {
       console.log("error registering user", err);
     });
 
   function* registerUser(action) {
+    // clear any existing error on the registration page
+    yield put({ type: "CLEAR_REGISTRATION_ERROR" });
 
-      // clear any existing error on the registration page
-      yield put({ type: "CLEAR_REGISTRATION_ERROR" });
+    // automatically log a user in after registration
+    yield put({ type: "LOGIN", payload: action.payload });
 
-      // automatically log a user in after registration
-      yield put({ type: "LOGIN", payload: action.payload });
-
-      // set to 'login' mode so they see the login screen
-      // after registration or after they log out
-      yield put({ type: "SET_TO_LOGIN_MODE" });
+    // set to 'login' mode so they see the login screen
+    // after registration or after they log out
+    yield put({ type: "SET_TO_LOGIN_MODE" });
   }
 };
 
