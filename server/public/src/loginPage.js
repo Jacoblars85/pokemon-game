722cc9355653c0fcd7ffc15a5523e1b61001a705
Jacob Larson <hockeybobber@comcast.net>
2025-02-28 21:57:43 -0600

    deleted the redux func

--- server/public/src/loginPage.js
+++ server/public/src/loginPage.js
@@ -30,28 +30,7 @@ const login = (event) => {
           console.log("error logging in user", err);
         });
 
-    function* loginUser(action) {
-      try {
-        // clear any existing error on the login page
-        yield put({ type: "CLEAR_LOGIN_ERROR" });
-
-        // after the user has logged in
-        // get the user information from the server
-        yield put({ type: "FETCH_USER" });
-      } catch (error) {
-        console.log("Error with user login:", error);
-        if (error.response.status === 401) {
-          // The 401 is the error status sent from passport
-          // if user isn't in the database or
-          // if the username and password don't match in the database
-          yield put({ type: "LOGIN_FAILED" });
-        } else {
-          // Got an error that wasn't a 401
-          // Could be anything, but most common cause is the server is not started
-          yield put({ type: "LOGIN_FAILED_NO_CODE" });
-        }
-      }
-    }
+    
   } else {
     dispatch({ type: "LOGIN_INPUT_ERROR" });
   }
