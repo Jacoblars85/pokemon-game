07c0b94db7895e4380b2f92e11f6aee1956d4549
Jacob Larson <hockeybobber@comcast.net>
2025-03-10 17:36:59 -0500

    fixed the style of starter 1 for second if

--- server/public/scripts/index.js
+++ server/public/scripts/index.js
@@ -1141,11 +1141,11 @@ function initBattle() {
     `;
   } else if (starters.length === 2) {
     document.getElementById("switchBox").innerHTML = `
-    <ul style=${{ padding: 0 }}>
+    <ul style=" padding: 0; ">
       <li>
         <img height="50" width="50" src=${starterOne.profile_pic} />
-        <p style=${{ ml: 25 }}>starter 1: ${starterOne.character_name}</p>
-         <p style=${{ ml: 25 }}>${starterOneHp}/${
+        <p style=" ml: 25; ">starter 1: ${starterOne.character_name}</p>
+         <p style=" ml: 25; ">${starterOneHp}/${
       starterOne.hp
     } hp | ${starterOneStamina}/${starterOne.stamina} stamina | ${
       starterOne.speed
@@ -1154,12 +1154,11 @@ function initBattle() {
         <button
           id="attackButton"
           className="starterOne"
-          style=${{
-            color: "black",
-            fontSize: 15,
-            fontFamily: "New Super Mario Font U",
-            borderColor: "black",
-          }}
+          style="
+            color: black;
+            font-size: 15;
+            border-color: black;
+          "
           disabled=${
             starter.isCurrentStarter === true
               ? true
