ebb55d9fd9f7f0f2bdda45c4f604447e5f7c4c97
Jacob Larson <hockeybobber@comcast.net>
2025-03-14 16:47:45 -0500

    got the players hit box to be able to go half in

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -501,14 +501,14 @@ function animate() {
         Math.min(
           player.position.y + player.height,
           boundary.position.y + boundary.height
-        ) - Math.max(player.position.y, boundary.position.y);
+        ) - Math.max(player.position.y, boundary.position.y - 34);
 
       if (
         player.position.x + player.width >= boundary.position.x &&
         player.position.x <= boundary.position.x + boundary.width &&
         player.position.y + player.height >= boundary.position.y - 3 &&
-        player.position.y <= boundary.position.y - 3 + boundary.height 
-        // overlappingArea > player.height / 2
+        player.position.y <= boundary.position.y - 3 + boundary.height &&
+        overlappingArea > player.height / 2
 
         // rectangularCollisions({
         //   rectangle1: player
