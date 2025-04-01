b267a6230529cbcb8ad23cccf63d5ef1e144d51a
Jacob Larson <hockeybobber@comcast.net>
2025-04-01 12:11:03 -0500

    formatted

--- public/scripts/battle.js
+++ public/scripts/battle.js
@@ -291,8 +291,9 @@ function resetBattleFunc() {
             // putWonBattle(reward);
 
             queue.push(() => {
-                document.getElementById("dialogueBox").innerHTML = "you won the battle!"
-              });
+              document.getElementById("dialogueBox").innerHTML =
+                "you won the battle!";
+            });
 
             queue.push(() => {
               fadeBackToExplore();
@@ -387,54 +388,58 @@ function resetBattleFunc() {
         document.getElementById("attackBox").style.display = "none";
         document.getElementById("switchBox").style.display = "none";
       } else if (e.target.innerHTML === "Run") {
-
         document.getElementById("dialogueBox").style.display = "block";
 
-        if (currentStarter.health + currentStarter.stamina + currentStarter.speed >= enemy.health + enemy.stamina + enemy.speed) {
-            console.log('starters stats are better');
+        if (
+          currentStarter.health +
+            currentStarter.stamina +
+            currentStarter.speed >=
+          enemy.health + enemy.stamina + enemy.speed
+        ) {
+          console.log("starters stats are better");
 
-            document.getElementById("dialogueBox").innerHTML = "you ran away successfully"
+          document.getElementById("dialogueBox").innerHTML =
+            "you ran away successfully";
 
-            queue.push(() => {
+          queue.push(() => {
             fadeBackToExplore();
-              });
-
+          });
         } else {
-            console.log('enemy stats are better');
+          console.log("enemy stats are better");
 
-            // queue.push(() => {
-                document.getElementById("dialogueBox").innerHTML = "you are not able to run"
-            //   });
+          // queue.push(() => {
+          document.getElementById("dialogueBox").innerHTML =
+            "you are not able to run";
+          //   });
 
-            queue.push(() => {
-                enemy.attack({
-                  attack: {},
-                  recipient: currentStarter,
-                  renderedSprites,
-                });
-    
-                if (currentStarter.health <= 0) {
-                  queue.push(() => {
-                    currentStarter.faint();
-                  });
-    
-                  if (starter.health <= 0 && starter2.health <= 0) {
-                    queue.push(() => {
-                      fadeBackToExplore();
-                    });
-                  }
-                  {
-                    queue.push(() => {
-                      document.getElementById("deadSwitchBox").style.display =
-                        "block";
-                    });
-                  }
-                }
-    
-                resetBattleFunc();
+          queue.push(() => {
+            enemy.attack({
+              attack: {},
+              recipient: currentStarter,
+              renderedSprites,
+            });
+
+            if (currentStarter.health <= 0) {
+              queue.push(() => {
+                currentStarter.faint();
               });
+
+              if (starter.health <= 0 && starter2.health <= 0) {
+                queue.push(() => {
+                  fadeBackToExplore();
+                });
+              }
+              {
+                queue.push(() => {
+                  document.getElementById("deadSwitchBox").style.display =
+                    "block";
+                });
+              }
+            }
+
+            resetBattleFunc();
+          });
         }
-        
       } else if (e.target.innerHTML === "Use Consumable") {
         document.getElementById("attackBox").style.display = "flex";
         document.getElementById("switchBox").style.display = "none";
