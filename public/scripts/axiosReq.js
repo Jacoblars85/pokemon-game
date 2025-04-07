be1326c8c833b92ff6a22edb17d2f365eb150bed
Jacob Larson <hockeybobber@comcast.net>
2025-04-07 17:07:38 -0500

    deleted the try in the log in

--- public/scripts/axiosReq.js
+++ public/scripts/axiosReq.js
@@ -19,37 +19,6 @@ function registerUser(action) {
 }
 
 function loginUser(action) {
-  try {
-    // clear any existing error on the login page
-    yield put({ type: "CLEAR_LOGIN_ERROR" });
-
-    const config = {
-      headers: { "Content-Type": "application/json" },
-      withCredentials: true,
-    };
-
-    // send the action.payload as the body
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    yield axios.post("/api/user/login", action.payload, config);
-
-    // after the user has logged in
-    // get the user information from the server
-    yield put({ type: "FETCH_USER" });
-  } catch (error) {
-    console.log("Error with user login:", error);
-    if (error.response.status === 401) {
-      // The 401 is the error status sent from passport
-      // if user isn't in the database or
-      // if the username and password don't match in the database
-      yield put({ type: "LOGIN_FAILED" });
-    } else {
-      // Got an error that wasn't a 401
-      // Could be anything, but most common cause is the server is not started
-      yield put({ type: "LOGIN_FAILED_NO_CODE" });
-    }
-  }
-
   const config = {
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
@@ -61,7 +30,7 @@ function loginUser(action) {
     data: {config: config, payload: action.payload },
   })
     .then((response) => {
-      console.log('logged in');
+      console.log('logged in', response);
       
     })
     .catch((err) => {
@@ -82,7 +51,7 @@ function logoutUser(action) {
     data: config,
   })
     .then((response) => {
-      console.log('logged out');
+      console.log('logged out', response);
       
     })
     .catch((err) => {
@@ -102,7 +71,7 @@ function fetchUser() {
     data: config,
   })
     .then((response) => {
-      console.log('got the user');
+      console.log('got the user', response);
       
     })
     .catch((err) => {
