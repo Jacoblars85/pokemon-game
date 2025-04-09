7e6e10284d5d319954f46746a7dcf2e96de53de7
Jacob Larson <hockeybobber@comcast.net>
2025-04-09 14:53:18 -0500

    commented out the inner html for the login

--- public/scripts/loginPage.js
+++ public/scripts/loginPage.js
@@ -32,68 +32,68 @@ const login = (event) => {
   }
 };
 
-document.getElementById("loginInterface").innerHTML = `
-<div>
-  <form className="formPanel" onSubmit={login}>
-    <h2
-      style={{
-        margin: "5px",
-        fontSize: "35px",
-        textShadow: "2px 2px 2px gray",
-      }}
-    >
-      Login
-    </h2>
-    <div style={{ height: "25px", width: "100%" }}>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-    </div>
-    <div>
-      <label htmlFor="username">
-        <input
-          margin="dense"
-          variant="standard"
-          type="text"
-          name="username"
-          label="username"
-          required
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </label>
-    </div>
-    <div>
-      <label htmlFor="password">
-        <input
-          margin="dense"
-          variant="standard"
-          type="password"
-          name="password"
-          label="password"
-          required
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-    </div>
-    <div>
-      <input className="btn" type="submit" name="submit" value="Log In" />
-    </div>
-  </form>
+// document.getElementById("loginInterface").innerHTML = `
+// <div>
+//   <form className="formPanel" onSubmit={login}>
+//     <h2
+//       style={{
+//         margin: "5px",
+//         fontSize: "35px",
+//         textShadow: "2px 2px 2px gray",
+//       }}
+//     >
+//       Login
+//     </h2>
+//     <div style={{ height: "25px", width: "100%" }}>
+//       {errors.loginMessage && (
+//         <h3 className="alert" role="alert">
+//           {errors.loginMessage}
+//         </h3>
+//       )}
+//     </div>
+//     <div>
+//       <label htmlFor="username">
+//         <input
+//           margin="dense"
+//           variant="standard"
+//           type="text"
+//           name="username"
+//           label="username"
+//           required
+//           value={username}
+//           onChange={(event) => setUsername(event.target.value)}
+//         />
+//       </label>
+//     </div>
+//     <div>
+//       <label htmlFor="password">
+//         <input
+//           margin="dense"
+//           variant="standard"
+//           type="password"
+//           name="password"
+//           label="password"
+//           required
+//           value={password}
+//           onChange={(event) => setPassword(event.target.value)}
+//         />
+//       </label>
+//     </div>
+//     <div>
+//       <input className="btn" type="submit" name="submit" value="Log In" />
+//     </div>
+//   </form>
 
-  <center>
-    <button
-      type="button"
-      className="btn btn_asLink"
-      onClick={() => {
-        history.push("/registration");
-      }}
-    >
-      Register
-    </button>
-  </center>
-</div>
-`;
+//   <center>
+//     <button
+//       type="button"
+//       className="btn btn_asLink"
+//       onClick={() => {
+//         history.push("/registration");
+//       }}
+//     >
+//       Register
+//     </button>
+//   </center>
+// </div>
+// `;
