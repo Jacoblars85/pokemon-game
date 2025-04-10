94a405fb07cf9388f583bfbb16099d57fc71a4a5
Jacob Larson <hockeybobber@comcast.net>
2025-04-10 18:37:35 -0500

    commented out old func and added the variable for register username

--- public/scripts/registerPage.js
+++ public/scripts/registerPage.js
@@ -1,38 +1,40 @@
 // let username = "";
 // let password = "";
 
+let getUsernameInput = document.getElementById('registerUsernameInput').value;
+
 // const errors = useSelector((store) => store.errors);
 
-const registerUser = (event) => {
-  event.preventDefault();
+// const registerUser = (event) => {
+//   event.preventDefault();
 
-  axios({
-    method: "POST",
-    url: "/api/user/register",
-    data: {
-      username: username,
-      password: password,
-    },
-  })
-    .then((response) => {
-      console.log("register worked?", response);
-    })
-    .catch((err) => {
-      console.log("error registering user", err);
-    });
+//   axios({
+//     method: "POST",
+//     url: "/api/user/register",
+//     data: {
+//       username: username,
+//       password: password,
+//     },
+//   })
+//     .then((response) => {
+//       console.log("register worked?", response);
+//     })
+//     .catch((err) => {
+//       console.log("error registering user", err);
+//     });
 
-//   function* registerUser(action) {
-//     // clear any existing error on the registration page
-//     yield put({ type: "CLEAR_REGISTRATION_ERROR" });
+// //   function* registerUser(action) {
+// //     // clear any existing error on the registration page
+// //     yield put({ type: "CLEAR_REGISTRATION_ERROR" });
 
-//     // automatically log a user in after registration
-//     yield put({ type: "LOGIN", payload: action.payload });
+// //     // automatically log a user in after registration
+// //     yield put({ type: "LOGIN", payload: action.payload });
 
-//     // set to 'login' mode so they see the login screen
-//     // after registration or after they log out
-//     yield put({ type: "SET_TO_LOGIN_MODE" });
-//   }
-};
+// //     // set to 'login' mode so they see the login screen
+// //     // after registration or after they log out
+// //     yield put({ type: "SET_TO_LOGIN_MODE" });
+// //   }
+// };
 
 // document.getElementById("registerInterface").innerHTML = `
 // <div>
