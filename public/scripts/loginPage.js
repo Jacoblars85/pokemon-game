a7f216528245a8a95d8ae884c7cb25223d631ca8
Jacob Larson <hockeybobber@comcast.net>
2025-04-11 23:08:00 -0500

    formatted

--- public/scripts/loginPage.js
+++ public/scripts/loginPage.js
@@ -1,21 +1,20 @@
-
-
 const login = (event) => {
   event.preventDefault();
 
-  let getUsernameInputLogin = document.getElementById("loginUsernameInput").value;
-let getPasswordInputLogin = document.getElementById("loginPasswordInput").value;
+  let getUsernameInputLogin =
+    document.getElementById("loginUsernameInput").value;
+  let getPasswordInputLogin =
+    document.getElementById("loginPasswordInput").value;
 
-document.getElementById("loginUsernameInput").value = "";
-document.getElementById("loginPasswordInput").value = "";
-
-
+  document.getElementById("loginUsernameInput").value = "";
+  document.getElementById("loginPasswordInput").value = "";
 
   if (getUsernameInputLogin && getPasswordInputLogin) {
-    loginUser({username: getUsernameInputLogin, password: getPasswordInputLogin })
-    
+    loginUser({
+      username: getUsernameInputLogin,
+      password: getPasswordInputLogin,
+    });
   } else {
-    console.log('username or password error');
-    
+    console.log("username or password error");
   }
 };
