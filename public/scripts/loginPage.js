165882362f7175e232c7d034df7aeda657cb666a
Jacob Larson <hockeybobber@comcast.net>
2025-04-11 23:05:36 -0500

    removed axios and added the func

--- public/scripts/loginPage.js
+++ public/scripts/loginPage.js
@@ -12,20 +12,8 @@ document.getElementById("loginPasswordInput").value = "";
 
 
   if (getUsernameInputLogin && getPasswordInputLogin) {
-    axios({
-      method: "POST",
-      url: "/api/user/login",
-      data: {
-        username: username,
-        password: password,
-      },
-    })
-      .then((response) => {
-        console.log("login worked?", response);
-      })
-      .catch((err) => {
-        console.log("error logging in user", err);
-      });
+    loginUser({username: getUsernameInputLogin, password: getPasswordInputLogin })
+    
   } else {
     dispatch({ type: "LOGIN_INPUT_ERROR" });
   }
