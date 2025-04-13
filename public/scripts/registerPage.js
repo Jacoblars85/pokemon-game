93e502c90191f306a380e82edc54022e077667c4
Jacob Larson <hockeybobber@comcast.net>
2025-04-12 21:07:06 -0500

    uncommented the register user func in register page

--- public/scripts/registerPage.js
+++ public/scripts/registerPage.js
@@ -6,33 +6,23 @@ document.getElementById("registerPasswordInput").value = "";
 
 // const errors = useSelector((store) => store.errors);
 
-// const registerUser = (event) => {
-//   event.preventDefault();
+const registerUser = (event) => {
+  event.preventDefault();
 
-//   axios({
-//     method: "POST",
-//     url: "/api/user/register",
-//     data: {
-//       username: username,
-//       password: password,
-//     },
-//   })
-//     .then((response) => {
-//       console.log("register worked?", response);
-//     })
-//     .catch((err) => {
-//       console.log("error registering user", err);
-//     });
+  axios({
+    method: "POST",
+    url: "/api/user/register",
+    data: {
+      username: username,
+      password: password,
+    },
+  })
+    .then((response) => {
+      console.log("register worked?", response);
+    })
+    .catch((err) => {
+      console.log("error registering user", err);
+    });
 
-// //   function* registerUser(action) {
-// //     // clear any existing error on the registration page
-// //     yield put({ type: "CLEAR_REGISTRATION_ERROR" });
 
-// //     // automatically log a user in after registration
-// //     yield put({ type: "LOGIN", payload: action.payload });
-
-// //     // set to 'login' mode so they see the login screen
-// //     // after registration or after they log out
-// //     yield put({ type: "SET_TO_LOGIN_MODE" });
-// //   }
-// };
+};
