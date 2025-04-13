0c61703de441d68647ae482ddab0d16dd817276f
Jacob Larson <hockeybobber@comcast.net>
2025-04-12 21:07:59 -0500

    moved variable names into register func

--- public/scripts/registerPage.js
+++ public/scripts/registerPage.js
@@ -1,13 +1,15 @@
-let getUsernameInput = document.getElementById("registerUsernameInput").value;
-let getPasswordInput = document.getElementById("registerPasswordInput").value;
 
-document.getElementById("registerUsernameInput").value = "";
-document.getElementById("registerPasswordInput").value = "";
 
 // const errors = useSelector((store) => store.errors);
 
 const registerUser = (event) => {
   event.preventDefault();
+
+  let getUsernameInput = document.getElementById("registerUsernameInput").value;
+let getPasswordInput = document.getElementById("registerPasswordInput").value;
+
+document.getElementById("registerUsernameInput").value = "";
+document.getElementById("registerPasswordInput").value = "";
 
   axios({
     method: "POST",
