8712a2c75a62a7cd544bff7b8f26c9a6b1fcd59f
Jacob Larson <hockeybobber@comcast.net>
2025-04-12 21:10:19 -0500

    formatted

--- public/scripts/registerPage.js
+++ public/scripts/registerPage.js
@@ -1,20 +1,16 @@
-
-
 // const errors = useSelector((store) => store.errors);
 
 const registerUser = (event) => {
   event.preventDefault();
 
   let getUsernameInput = document.getElementById("registerUsernameInput").value;
-let getPasswordInput = document.getElementById("registerPasswordInput").value;
+  let getPasswordInput = document.getElementById("registerPasswordInput").value;
 
-document.getElementById("registerUsernameInput").value = "";
-document.getElementById("registerPasswordInput").value = "";
+  document.getElementById("registerUsernameInput").value = "";
+  document.getElementById("registerPasswordInput").value = "";
 
-registerUserPost({
-  username: getUsernameInput,
-  password: getPasswordInput,
-})
-
-
+  registerUserPost({
+    username: getUsernameInput,
+    password: getPasswordInput,
+  });
 };
