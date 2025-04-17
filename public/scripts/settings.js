fb9352eb0fb0a2c5f65a394b13751090dbf53395
Jacob Larson <hockeybobber@comcast.net>
2025-04-16 22:08:43 -0500

    added logs

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,11 +1,14 @@
 function deleteUser() {
+    console.log('trying to delete user');
     
 }
 
 function goToMainMenu() {
+    console.log('trying to go to main menu');
     
 }
 
 function changeName() {
+    console.log('trying to change users name');
     
 }
