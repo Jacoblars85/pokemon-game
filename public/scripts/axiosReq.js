a399c6c61e74096a311f9bdf6acece81565b8f88
Jacob Larson <hockeybobber@comcast.net>
2025-04-17 15:02:05 -0500

    deleted the old try in hange username func

--- public/scripts/axiosReq.js
+++ public/scripts/axiosReq.js
@@ -95,19 +95,7 @@ function deleteUser() {
 }
 
 function changeUsername(newName) {
-    try {
-        const response = yield axios({
-            method: 'PUT',
-            url: `/api/user/change`,
-            data: {newName: action.payload}
-        })
-        yield put({
-            type: 'FETCH_USER',
-        })
-    } catch (error) {
-      alert('you cant do that')
-        console.log('Unable to update username from server', error);
-    }
+
 
     axios({
         method: "PUT",
