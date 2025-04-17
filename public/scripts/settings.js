77bff08dc1229b945c4275c6063b2211fa34e0d2
Jacob Larson <hockeybobber@comcast.net>
2025-04-17 15:28:59 -0500

    formatted

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,14 +1,11 @@
 function deleteUserDialog() {
-    console.log('trying to delete user');
-    
+  console.log("trying to delete user");
 }
 
 function goToMainMenu() {
-    console.log('trying to go to main menu');
-    
+  console.log("trying to go to main menu");
 }
 
 function changeUsernameDialog() {
-    console.log('trying to change users name');
-    
+  console.log("trying to change users name");
 }
