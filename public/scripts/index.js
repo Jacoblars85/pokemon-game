b4eed7f10d69890b844432721b94dc9f6339a683
Jacob Larson <hockeybobber@comcast.net>
2025-04-26 00:03:42 -0500

    formatted

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,4 @@
 function goToSettings() {
-    window.location.href =
+  window.location.href =
     "/Users/jacoblars85/Desktop/code/fun/projects/pokemon-game/public/scripts/settingsPage/settings.html";
 }
