55ca4b76708861bd2a9c92e3718369b15e1caf56
Jacob Larson <hockeybobber@comcast.net>
2025-05-03 22:02:35 -0500

    added open and close settings diaglod to the index

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,3 +2,15 @@ function goToSettings() {
   window.location.href =
     "/Users/jacoblars85/Desktop/code/fun/projects/pokemon-game/public/scripts/settingsPage/settings.html";
 }
+
+function openSettingsDialog() {
+  moving = false;
+
+  document.getElementById("settingsInterfacePopUp").style.display = "flex";
+}
+
+function closeSettingsDialog() {
+  moving = true;
+
+  document.getElementById("settingsInterfacePopUp").style.display = "none";
+}
