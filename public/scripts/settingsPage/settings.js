0742e47eba9be68bf03ede6c25337068351de91f
Jacob Larson <hockeybobber@comcast.net>
2025-05-05 16:24:13 -0500

    formatted

--- public/scripts/settingsPage/settings.js
+++ public/scripts/settingsPage/settings.js
@@ -34,23 +34,23 @@ function changeUsernameDialog() {
   
   <div 
     style="
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        row-gap: 20px;
-        ">
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      row-gap: 20px;
+      ">
 
     <input></input>
     <button class="btn">change username</button>
   </div>
 
   <button
-          class="btn"
-          style="position: absolute; top: 0.5%; left: 0.5%; margin: 0"
-          onclick="resetSettingsBody()"
-        >
-          <--
-        </button>
+    class="btn"
+    style="position: absolute; top: 0.5%; left: 0.5%; margin: 0"
+    onclick="resetSettingsBody()"
+  >
+    <--
+  </button>
 
 `;
 }
@@ -89,5 +89,5 @@ function closeSettingsDialog() {
 
   document.getElementById("settingsInterfacePopUp").style.display = "none";
 
-  resetSettingsBody()
+  resetSettingsBody();
 }
