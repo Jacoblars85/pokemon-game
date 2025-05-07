b8df842746106585faa9d06aefc2b62aee06c6d0
Jacob Larson <hockeybobber@comcast.net>
2025-05-06 20:46:12 -0500

    brought back the cheast zones map

--- public/scripts/gamePage/explore.js
+++ public/scripts/gamePage/explore.js
@@ -25,11 +25,11 @@ for (let i = 0; i < battleZonesArray.length; i += 235) {
   battleZonesMap.push(battleZonesArray.slice(i, 235 + i));
 }
 
-// const cheastZonesMap = []
+const cheastZonesMap = []
 
-// for (let i = 0; i < cheastZonesArray.length; i += 235) {
-//   cheastZonesMap.push(cheastZonesArray.slice(i, 235 + i));
-// }
+for (let i = 0; i < cheastZonesArray.length; i += 235) {
+  cheastZonesMap.push(cheastZonesArray.slice(i, 235 + i));
+}
 
 const offset = {
   x: -4767.5,
