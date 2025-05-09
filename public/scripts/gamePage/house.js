6d50b5651eea6be3413299d8c086518e31d8339b
Jacob Larson <hockeybobber@comcast.net>
2025-05-08 22:17:13 -0500

    formatted and changed the house background draw

--- public/scripts/gamePage/house.js
+++ public/scripts/gamePage/house.js
@@ -2,24 +2,20 @@ const houseBackgroundImage = new Image();
 houseBackgroundImage.src = "./img/backgroundImg/battleBackground.png";
 
 const houseBackground = new Sprite({
-    position: {
-      x: 0,
-      y: 0,
-    },
-    image: houseBackgroundImage,
-  });
+  position: {
+    x: 0,
+    y: 0,
+  },
+  image: houseBackgroundImage,
+});
 
 let houseAnimationId;
 
-
 function animateHouse() {
+  houseAnimationId = window.requestAnimationFrame(animateHouse);
+  houseBackground.draw();
 
-        houseAnimationId = window.requestAnimationFrame(animateHouse);
-        // battleBackground.draw();
-
-        player.draw();
+  player.draw();
   moving = true;
   player.animate = false;
-
-      
 }
