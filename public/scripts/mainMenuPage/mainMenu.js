765005a106ed644ca252ca86149ae8a216720add
Jacob Larson <hockeybobber@comcast.net>
2025-05-30 10:41:07 -0500

    added setting of the nav headers

---
+++ public/scripts/mainMenuPage/mainMenu.js
@@ -0,0 +1,5 @@
+fetchUser()
+
+document.getElementById("usernameNavHeader").innerHTML = user.username
+document.getElementById("userLevelNavHeader").innerHTML = user.xp_level
+document.getElementById("usersCoinsNavHeader").innerHTML = user.coins
