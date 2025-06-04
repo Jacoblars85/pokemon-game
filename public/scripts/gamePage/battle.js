f8b839ef5831c6fd8b9f558a3e7074af1f4654e8
Jacob Larson <hockeybobber@comcast.net>
2025-06-03 22:54:43 -0500

    fixed other speed side if for both dead

--- public/scripts/gamePage/battle.js
+++ public/scripts/gamePage/battle.js
@@ -365,9 +365,9 @@ function resetBattleFunc() {
             });
 
             if (
-              starter.health <= 0 && starterTwo != null
+              starter.health <= 0 && (starterTwo != null
                 ? starter2.health <= 0
-                : currentStarter.health <= 0
+                : currentStarter.health <= 0)
             ) {
               queue.push(() => {
                 document.getElementById("dialogueBox").innerHTML =
