32fe68db956e9cb8035cf8df90e4094513570bc9
Jacob Larson <hockeybobber@comcast.net>
2025-06-06 22:38:02 -0500

    commented out old code again and added 2 new if for num of shwakes

--- public/scripts/classes.js
+++ public/scripts/classes.js
@@ -264,50 +264,55 @@ class Character extends Sprite {
             },
           });
             
-          } else {
-console.log('didnt catch');
+          } else if (numOfShakes === 1) {
+console.log('didnt catch 1 shake');
+
+            
+          } else if (numOfShakes === 2) {
+
+console.log('didnt catch 2 shake');
 
 
 
           }
 
-          gsap.to(throwableFx.position, {
-            x: throwableFx.position.x + 10,
-            yoyo: true,
-            repeat: 5,
-            duration: 0.08,
-            delay: 1,
-            onComplete: () => {
-              gsap.to(throwableFx.position, {
-                x: throwableFx.position.x + 10,
-                yoyo: true,
-                repeat: 5,
-                duration: 0.08,
-                delay: 2,
-                onComplete: () => {
-                  gsap.to(throwableFx.position, {
-                    x: throwableFx.position.x + 10,
-                    yoyo: true,
-                    repeat: 5,
-                    duration: 0.08,
-                    delay: 2,
-                    onComplete: () => {
-                      gsap.to(throwableFx.position, {
-                        delay: 2,
-                        onComplete: () => {
-                          gsap.to(recipient, {
-                            opacity: 1,
-                            duration: 0.5,
-                          });
-                          renderedSprites.splice(2, 1);
-                        },
-                      });
-                    },
-                  });
-                },
-              });
-            },
-          });
+          // gsap.to(throwableFx.position, {
+          //   x: throwableFx.position.x + 10,
+          //   yoyo: true,
+          //   repeat: 5,
+          //   duration: 0.08,
+          //   delay: 1,
+          //   onComplete: () => {
+          //     gsap.to(throwableFx.position, {
+          //       x: throwableFx.position.x + 10,
+          //       yoyo: true,
+          //       repeat: 5,
+          //       duration: 0.08,
+          //       delay: 2,
+          //       onComplete: () => {
+          //         gsap.to(throwableFx.position, {
+          //           x: throwableFx.position.x + 10,
+          //           yoyo: true,
+          //           repeat: 5,
+          //           duration: 0.08,
+          //           delay: 2,
+          //           onComplete: () => {
+          //             gsap.to(throwableFx.position, {
+          //               delay: 2,
+          //               onComplete: () => {
+          //                 gsap.to(recipient, {
+          //                   opacity: 1,
+          //                   duration: 0.5,
+          //                 });
+          //                 renderedSprites.splice(2, 1);
+          //               },
+          //             });
+          //           },
+          //         });
+          //       },
+          //     });
+          //   },
+          // });
         },
       });
     } else if (item.item_type === "consumable") {
