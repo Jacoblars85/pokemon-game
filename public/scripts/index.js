29f91c87db0f30d5a9f1e994516f3ba2998fe369
Jacob Larson <hockeybobber@comcast.net>
2025-06-20 23:05:24 -0500

    removed settings func and moved it into settings js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,15 +4,3 @@ function goToSettings() {
   window.location.href =
     "/Users/jacoblars85/Desktop/code/fun/projects/pokemon-game/public/scripts/settingsPage/settings.html";
 }
-
-function openSettingsDialog() {
-  moving = false;
-
-  document.getElementById("settingsInterfacePopUp").style.display = "flex";
-}
-
-function closeSettingsDialog() {
-  moving = true;
-
-  document.getElementById("settingsInterfacePopUp").style.display = "none";
-}
