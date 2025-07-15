8cfe987b0cb3f8673c40f016524cd80676a9e19d
Jacob Larson <hockeybobber@comcast.net>
2025-07-14 22:33:48 -0500

    commented out old axios

--- public/scripts/axiosReq.js
+++ public/scripts/axiosReq.js
@@ -718,23 +718,23 @@ function equipItem(item, starter) {
     });
 }
 
-  function* removeItem(action) {
+  function removeItem(action) {
     // console.log('action.payload', action.payload);
-    try {
-      const response = yield axios({
-        method: 'PUT',
-        url: `/api/inventory/remove/item`,
-        data: action.payload
-      })
-      yield put({
-        type: 'SAGA_FETCH_IVENTORY',
-      })
-      yield put({
-        type: 'SAGA_FETCH_CHARACTERS',
-      })
-    } catch (error) {
-      console.log('Unable to equip item to server', error);
-    }
+    // try {
+    //   const response = yield axios({
+    //     method: 'PUT',
+    //     url: `/api/inventory/remove/item`,
+    //     data: action.payload
+    //   })
+    //   yield put({
+    //     type: 'SAGA_FETCH_IVENTORY',
+    //   })
+    //   yield put({
+    //     type: 'SAGA_FETCH_CHARACTERS',
+    //   })
+    // } catch (error) {
+    //   console.log('Unable to equip item to server', error);
+    // }
 
       axios({
     method: "PUT",
