ac90c071b91ce58c10604c2d639d3e673db583ed
Jacob Larson <hockeybobber@comcast.net>
2025-07-19 22:40:19 -0500

    added current and max hp and stamina to get user characters route

--- server/routes/character.router.js
+++ server/routes/character.router.js
@@ -113,17 +113,19 @@ router.get("/user/characters", (req, res) => {
 
   const query = `
 SELECT "user_characters"."id" as "id",
-		"user_characters"."user_id" as "user_id",
-		"user_characters"."character_id",
+		    "user_characters"."user_id" as "user_id",
+		    "user_characters"."character_id",
+        "user_characters"."current_hp" as "hp",
+        "user_characters"."current_stamina" as "stamina",
+        "user_characters"."max_hp",
+        "user_characters"."max_stamina",
         "user_characters"."starter_1",
         "user_characters"."starter_2",
         "user_characters"."new",
         "user_characters"."nickname",
         "user_characters"."xp_level",
-		"characters"."character_name",
-		"characters"."profile_pic",
-		"characters"."hp",
-		"characters"."stamina",
+		    "characters"."character_name",
+		    "characters"."profile_pic",
         "characters"."speed",
         "characters"."battle_pic",
         "attacks"."id" as "attacks_id",
@@ -145,7 +147,7 @@ SELECT "user_characters"."id" as "id",
         "items"."item_speed",
         "items"."item_damage",
         "items"."item_cost",
-    	"items"."item_color"
+    	  "items"."item_color"
  FROM "user_characters" 
 	INNER JOIN "characters"
     	ON "user_characters"."character_id" = "characters"."id"
