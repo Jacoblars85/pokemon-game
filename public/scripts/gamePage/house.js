a7d0721f43657bbe63d21276b3a9197383b49415
Jacob Larson <hockeybobber@comcast.net>
2025-08-21 23:04:30 -0500

    formatted

--- public/scripts/gamePage/house.js
+++ public/scripts/gamePage/house.js
@@ -202,17 +202,16 @@ function showCharacterDetails(character, context = "") {
 
   console.log("character", character);
 
-  if (condition) {
-      document.getElementById("removeStarterButton").dataset.characterId =
-    character.id;
+  if (character.item_id) {
+    document.getElementById("removeStarterButton").dataset.characterId =
+      character.id;
 
-  document.getElementById("removeStarterButton").dataset.itemId =
-    character.item_id;
+    document.getElementById("removeStarterButton").dataset.itemId =
+      character.item_id;
 
-  document.getElementById("detailItemName").textContent = character.item_name;
-  document.getElementById("detailItemImage").src = character.item_pic;
+    document.getElementById("detailItemName").textContent = character.item_name;
+    document.getElementById("detailItemImage").src = character.item_pic;
   }
-
 
   let starterButtonContainer = document.getElementById(
     "starterButtonContainer"
