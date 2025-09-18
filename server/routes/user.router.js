137e4ac0b3bc46073af5af084f4a0c3ffa9fd182
Jacob Larson <hockeybobber@comcast.net>
2025-09-18 10:16:42 -0500

    added another chest

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -118,7 +118,8 @@ router.post("/register", (req, res, next) => {
               ($1, 7),
               ($1, 8),
               ($1, 9),
-              ($1, 10);
+              ($1, 10),
+              ($1, 11);
           `;
                   const insertNewUserValues = [createdUserId];
 
