3fc41f9e503492cf716cc90cba695e3422648fff
Jacob Larson <hockeybobber@comcast.net>
2025-10-23 10:05:37 -0500

    added new if that adds additianal power to attack if the character and attack are the same type

--- public/scripts/classes.js
+++ public/scripts/classes.js
@@ -425,13 +425,19 @@ class Character extends Sprite {
 
     console.log("attack before the if", attack.attack_damage);
 
-    if (attack.attack_type_effective === recipient.character_type_id) {
-      console.log("attack is effectiive");
-      attack.attack_damage += 20;
+if (attack.attack_type_id === this.character_type_id) {
+      console.log("attack is the same type as the attacker so + 10");
+      attack.attack_damage += 10;
     }
 
+    if (attack.attack_type_effective === recipient.character_type_id) {
+      console.log("attack is effectiive against recipient so + 20");
+      attack.attack_damage += 20;
+    }
+    
+
     if (attack.attack_type_weakness === recipient.character_type_id) {
-      console.log("attack is weak");
+      console.log("attack is weak against recipient so - 20");
       attack.attack_damage -= 20;
     }
 
