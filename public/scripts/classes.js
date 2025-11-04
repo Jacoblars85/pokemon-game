0c86ece55f1587d8f6fcef7266257220c887ce66
Jacob Larson <hockeybobber@comcast.net>
2025-11-03 19:08:08 -0600

    added log for attack

--- public/scripts/classes.js
+++ public/scripts/classes.js
@@ -420,6 +420,8 @@ class Character extends Sprite {
       }
     }
 
+    console.log('attack', attack);
+    
     console.log("attack before the if", attack.attack_damage);
 
     if (attack.attack_type_id === this.character_type_id) {
