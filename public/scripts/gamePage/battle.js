65c247c86fe8833e301a8e10d54707f17611769f
Jacob Larson <hockeybobber@comcast.net>
2025-11-03 18:53:29 -0600

    changed the new attacks

--- public/scripts/gamePage/battle.js
+++ public/scripts/gamePage/battle.js
@@ -646,7 +646,7 @@ function initBattle() {
     character_type_weakness: enemyOne.character_type_weakness,
     level: enemyOne.xp_level,
     fx_img: enemyAttackStats.fx_img,
-    attackStats: enemyAttackStats,
+    attacks: enemyAttackStats,
     max_frames: enemyAttackStats.max_frames,
     hold_time: enemyAttackStats.hold_time,
   });
