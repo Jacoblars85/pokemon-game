8d80586c6b3e24fcd56a64b166005d4b091d3c10
Jacob Larson <hockeybobber@comcast.net>
2025-11-14 20:54:30 -0600

    fixed error in items table

--- database.sql
+++ database.sql
@@ -325,14 +325,14 @@ INSERT INTO "items"
 	('short sword', 'held', 0, 0, 0, 15, 0, '../../images/held/ShortSwordSprite.png', 100, '#000000'),
 	('sword', 'held', 0, 0, 0, 20, 0, '../../images/held/SwordSprite.png', 150, '#000000'),
 	('book', 'held', 0, 20, 10, 0, 0, '../../images/held/BookSprite.png', 120, '#000000'),
-	('magic wand', 'held', 15, 0, 0, 0, 0, '../../images/held/MagicWandSprite.png', 150, '#000000')
-	('slash', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
-	('fireball', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
-	('leaf blade', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
-	('waterball', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
-	('rock slide', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
-	('thunder', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
-	('curse', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000')
+	('magic wand', 'held', 15, 0, 0, 0, 0, '../../images/held/MagicWandSprite.png', 150, '#000000'),
+	('slash', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
+	('fireball', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
+	('leaf blade', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
+	('waterball', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
+	('rock slide', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
+	('thunder', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
+	('curse', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000'),
 	('spirit bomb', 'tm', 0, 0, 0, 0, 0, '../../images/', 250, '#000000');
 		
 INSERT INTO "rewards" 
